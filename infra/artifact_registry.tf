5256f08a46ab7cc6559b8dbaa17a978430d16675
Ricardo Morales <ricardo.morales.b@gmail.com>
2026-05-29 15:53:24 -0700

    Import existing Artifact Registry repo into Terraform state

--- infra/artifact_registry.tf
+++ infra/artifact_registry.tf
@@ -1,8 +1,13 @@
+import {
+  to = google_artifact_registry_repository.synthlog
+  id = "projects/${var.project_id}/locations/${var.region}/repositories/synthlog"
+}
+
 resource "google_artifact_registry_repository" "synthlog" {
   repository_id = "synthlog"
   location      = var.region
   format        = "DOCKER"
-  description   = "Docker images for synthlog synthetic Okta log generator"
+  description   = "Docker images for synthlog synthetic log generator"
 }
 
 locals {
